70ee9cca310d3efdb53ecbc3d9ad4e9f802db890
Nasir Ahmed <nasir.ahmed@bizzntek.com>
2023-02-14 12:39:29 +0600

    feat: add guess number and check

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,59 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const data1 = [1, 2, 3];
-const data2 = [1, 2, 3, 4];
-console.log(`...${data1[0]}°C in 1 days ...${data1[1]}°C in 2 days ...${data1[2]}°C in 3 days`);
+//#region Elements
+const checkButtonEl = document.querySelector(".check");
+const guessInputEl = document.querySelector(".guess");
+const messageParaEl = document.querySelector(".message");
+const numberDivEl = document.querySelector(".number");
+const scoreSpanEl = document.querySelector(".score");
+const bodyEl = document.querySelector("body");
+//#endregion
+//#region Functions
+const generateRandomNumber = function (from = 1, to = 20) {
+    const number = Math.trunc(Math.random() * (to - from + 1)) + from;
+    return number;
+};
+const showMessage = function (content) {
+    messageParaEl.textContent = content;
+};
+//#endregion
+const secretNumber = generateRandomNumber();
+let score = 20;
+checkButtonEl.addEventListener("click", function () {
+    const guessNumber = +guessInputEl.value;
+    const hasValue = !!guessInputEl.value;
+    // When there is no input
+    if (!hasValue) {
+        showMessage("⛔ No Number");
+    }
+    //   When win
+    else if (guessNumber === secretNumber) {
+        showMessage("🏆Correct Guess!!!");
+        numberDivEl.textContent = secretNumber.toString();
+        bodyEl.style.backgroundColor = "#60b347";
+        numberDivEl.style.width = "30rem";
+    }
+    //   When guess high
+    else if (guessNumber > secretNumber) {
+        if (score > 1) {
+            showMessage("📉Too high!!!");
+            score--;
+            scoreSpanEl.textContent = score.toString();
+        }
+        else {
+            showMessage("💥You lost the game!!!");
+            scoreSpanEl.textContent = String(0);
+        }
+    }
+    //   When guess low
+    else if (guessNumber < secretNumber) {
+        if (score > 1) {
+            showMessage("📈Too Low!!!");
+            score--;
+            scoreSpanEl.textContent = score.toString();
+        }
+        else {
+            showMessage("💥You lost the game!!!");
+            scoreSpanEl.textContent = String(0);
+        }
+    }
+});
--- src/script.ts
+++ src/script.ts
@@ -1,8 +1,64 @@
-const data1: number[] = [1, 2, 3];
-const data2: number[] = [1, 2, 3, 4];
+//#region Elements
+const checkButtonEl = document.querySelector(".check") as HTMLButtonElement;
+const guessInputEl = document.querySelector(".guess") as HTMLInputElement;
+const messageParaEl = document.querySelector(
+  ".message"
+) as HTMLParagraphElement;
+const numberDivEl = document.querySelector(".number") as HTMLDivElement;
+const scoreSpanEl = document.querySelector(".score") as HTMLSpanElement;
+const bodyEl = document.querySelector("body") as HTMLBodyElement;
+//#endregion
 
-console.log(
-  `...${data1[0]}°C in 1 days ...${data1[1]}°C in 2 days ...${data1[2]}°C in 3 days`
-);
+//#region Functions
+const generateRandomNumber = function (
+  from: number = 1,
+  to: number = 20
+): number {
+  const number = Math.trunc(Math.random() * (to - from + 1)) + from;
+  return number;
+};
 
-export {};
+const showMessage = function (content: string): void {
+  messageParaEl.textContent = content;
+};
+//#endregion
+const secretNumber = generateRandomNumber();
+let score = 20;
+
+checkButtonEl.addEventListener("click", function () {
+  const guessNumber = +guessInputEl.value;
+  const hasValue = !!guessInputEl.value;
+  // When there is no input
+  if (!hasValue) {
+    showMessage("⛔ No Number");
+  }
+  //   When win
+  else if (guessNumber === secretNumber) {
+    showMessage("🏆Correct Guess!!!");
+    numberDivEl.textContent = secretNumber.toString();
+    bodyEl.style.backgroundColor = "#60b347";
+    numberDivEl.style.width = "30rem";
+  }
+  //   When guess high
+  else if (guessNumber > secretNumber) {
+    if (score > 1) {
+      showMessage("📉Too high!!!");
+      score--;
+      scoreSpanEl.textContent = score.toString();
+    } else {
+      showMessage("💥You lost the game!!!");
+      scoreSpanEl.textContent = String(0);
+    }
+  }
+  //   When guess low
+  else if (guessNumber < secretNumber) {
+    if (score > 1) {
+      showMessage("📈Too Low!!!");
+      score--;
+      scoreSpanEl.textContent = score.toString();
+    } else {
+      showMessage("💥You lost the game!!!");
+      scoreSpanEl.textContent = String(0);
+    }
+  }
+});
